8b10802a2a89f837dd97de4bca9e11ab54b6b6c2
Wallef Aquino <wallef.aquino@aisdigital.com.br>
2020-03-01 20:12:44 -0300

    Configração Eureka Server

---
+++ src/main/kotlin/com/walleftech/confiboieurekaserver/ConfiboiEurekaServerApplication.kt
@@ -0,0 +1,13 @@
+package com.walleftech.confiboieurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class ConfiboiEurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfiboiEurekaServerApplication>(*args)
+}
